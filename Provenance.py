6033f6e7c8c10c8e26528974597cda5c9e21549f
Ryan Cervantes <rxc3202@rit.edu>
2020-01-08 16:31:11 -0500

    added some argument parsing

--- Provenance.py
+++ Provenance.py
@@ -4,9 +4,12 @@ Author:			Ryan Cervantes
 Description:	The main running file for the Provenance C2 
 """
 
+import argparse
 import dnslib
-from socketserver import UDPServer, BaseRequestHandler
 import threading
+import utils
+import sys
+from socketserver import UDPServer, BaseRequestHandler
 from machine import IndividualClientHandler
 
 
@@ -15,6 +18,9 @@ class ProvenanceServer(UDPServer):
 	def __init__(self, server_address, RequestHandlerClass, bind_and_activate=True):
 		super().__init__(server_address, RequestHandlerClass, bind_and_activate)
 		self.machines = {}
+		self.whitelist = utils.parse_whitelist(args.whitelist)
+		print(self.whitelist)
+		self.blacklist = []
 
 
 	def get_request(self):
@@ -40,14 +46,38 @@ class ProvenanceServer(UDPServer):
 		addr, port = client_address
 		return self.machines[addr].handle()
 
+
+def parse_arguments():
+	parser = argparse.ArgumentParser(description="The Provenance C2 Server")
+	# Positional Arguments
+	parser.add_argument("ui",
+					 choices=["gui", "cli"],
+					 help="Run the server as a command line application or spawn a GUI")
+	parser.add_argument("interface", help="The interface the C2 Sever is located on")
+	parser.add_argument("port", type=int, default=53, help="The port on which the server listens")
+	# Optional Arguments
+	parser.add_argument("-v", "--verbose", action="count", help="""Use -v to display [INFO].
+																   Use -vv to display [INFO] and [DEBUG].""")
+	parser.add_argument("--discovery", action="store_true")
+	parser.add_argument("--blacklist", nargs=1, help="Don't interact with these IPs")
+	parser.add_argument("--whitelist", nargs=1, type=argparse.FileType('r'), default=sys.stdin, help="""Only interact with these IPs.
+																			Use ':' to delimit IPs to hostnames (<ip>:<hostname>).
+																			Takes precedence over blacklist. If no file is given,
+																			then IPs will be read in from the commandline""")
+	return parser.parse_args()
+
+
 def main():
-	socket = ("127.0.0.1", 53)	
+	global args
+	args = parse_arguments()
+	print(f"namespace: {args}")
+	socket = (args.interface, args.port)	
 	with ProvenanceServer(socket, IndividualClientHandler) as server:
 		try:
 			print(f"Starting Server on {socket[0]}:{socket[1]}...")
-			server.serve_forever()
+			# server.serve_forever()
 		except KeyboardException:
-			pass
+			sys.exit(0)
 		finally:
 			server.shutdown()
 
---
+++ utils.py
@@ -0,0 +1,16 @@
+"""
+filename: utils.py
+author: Ryan Cervantes (rxc3202)
+description: utility functions
+"""
+
+def parse_whitelist(filename):
+	whitelist = []
+	print(filename)
+	# with open(filename, 'r') as file:
+	# 	for line in file:
+	# 		ip, hostname = line.strip().split(':')
+	# 		whitelist.append((ip, hostname))
+	return whitelist
+			
+
